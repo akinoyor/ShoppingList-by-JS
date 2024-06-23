69356a3bd5372023818755eb685b997da63bff33
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-06-23 07:23:07 +0000

    [add]localStorage機能追加

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const $itemName = document.getElementById('item_name');
 const $itemQuantity = document.getElementById('item_quantity');
 const $select = document.getElementById('list_tag');
 let selectedTag = "一覧";
-const $itemMemo = document.getElementById('memo');
+const $itemMemo = document.getElementById('item_memo');
 const $registration = document.getElementById('registration');
 let itemObj = {};
 const $itemsList = document.getElementById('items_list');
+const localItems = JSON.parse(localStorage.getItem('items'));
+if(localItems){
+  localItems.forEach(item =>{
+    add(item);
+  });
+};
 
 $select.onchange = function(){
   selectedTag = this.value;
@@ -21,6 +27,7 @@ $registration.addEventListener('click', () => {
       itemObj['memo'] = $itemMemo.value;
       add(itemObj);
       reset();
+      save();
     }else{
       window.alert('数量は0個以上で入力してください');
     };
@@ -40,42 +47,59 @@ function add(itemObj){
   const divMemo = document.createElement('div');
   const buttonFinish = document.createElement('button');
   const buttonFinishRemove = document.createElement('button');
+  const buttonDelete = document.createElement('button');
   const row2 = document.createElement('div');
-  divItem.classList.add('col-7', 'me-2', 'border', 'border-dark');
-  divItem.innerText = itemObj['name'];
+  const containers = [
+    { name: divItem, classes: ['col-7','me-2','border','border-dark', 'name'], id: 'name' },
+    { name: divQuantity, classes: ['col', 'me-2', 'border', 'border-dark', 'quantity'], id: 'quantity' },
+    { name: divTag, classes: ['col-3','border', 'border-dark', 'tag'], id: 'tag' },
+    { name: divMemo, classes: ['col', 'me-2', 'border', 'border-dark', 'memo'], id: 'memo' }
+  ];
 
-  divQuantity.classList.add('col', 'me-2', 'border', 'border-dark');
-  divQuantity.innerText = itemObj['quantity'];
+  containers.forEach(container =>{
+    container.name.classList.add( ...container.classes);
+    container.name.innerText = itemObj[container.id];
+  });
 
-  divTag.classList.add('col-3','border', 'border-dark');
-  divTag.innerText = itemObj['tag'];
+  buttonFinish.classList.add('col-3', 'btn', 'btn-primary');
+  buttonFinish.innerText = '完了';
+  buttonFinish.addEventListener('click', () =>{
+    divItem.classList.add('text-decoration-line-through');
+    buttonFinishRemove.classList.remove('none');
+    buttonDelete.classList.remove('none');
+    buttonFinish.classList.add('none');
+    save();
+  });
+  buttonFinishRemove.classList.add('col-1', 'btn', 'btn-warning', 'none');
+  buttonFinishRemove.innerText = '取消';
+  buttonFinishRemove.addEventListener('click', () =>{
+    divItem.classList.remove('text-decoration-line-through');
+    buttonFinishRemove.classList.add('none');
+    buttonDelete.classList.add('none');
+    buttonFinish.classList.remove('none');
+    save();
+  });
+  buttonDelete.classList.add('col-2', 'btn', 'btn-danger', 'none');
+  buttonDelete.innerText = '削除';
+  buttonDelete.addEventListener('click', () =>{
+    div.remove();
+    save();
+  });
 
-  contents = [divItem, divQuantity, divTag];//挿入内容
+  if(itemObj.completed){
+    divItem.classList.add('text-decoration-line-through');
+    buttonFinishRemove.classList.remove('none');
+    buttonDelete.classList.remove('none');
+    buttonFinish.classList.add('none');
+  };
 
   row1.classList.add('row');
+  contents = [divItem, divQuantity, divTag];//挿入内容
   contents.forEach(content =>{
     row1.appendChild(content);
   });
 
-  divMemo.classList.add('col', 'me-2', 'border', 'border-dark');
-  divMemo.innerText = itemObj['memo'];
-
-  buttonFinish.classList.add('col-3', 'btn', 'btn-primary');
-  buttonFinish.innerText = '完了';
-  buttonFinish.addEventListener('click', () => {
-    divItem.classList.add('text-decoration-line-through');
-    buttonFinishRemove.classList.remove('none');
-    buttonFinish.classList.add('none');
-  });
-  buttonFinishRemove.classList.add('col-3', 'btn', 'btn-warning', 'none');
-  buttonFinishRemove.innerText = '完了取り消し';
-  buttonFinishRemove.addEventListener('click', () => {
-    divItem.classList.remove('text-decoration-line-through');
-    buttonFinishRemove.classList.add('none');
-    buttonFinish.classList.remove('none');
-  });
-
-  contents = [divMemo, buttonFinish, buttonFinishRemove];
+  contents = [divMemo, buttonFinish, buttonFinishRemove, buttonDelete];
 
   row2.classList.add('row', 'my-1');
   contents.forEach(content =>{
@@ -89,11 +113,26 @@ function add(itemObj){
   });
 
   $itemsList.appendChild(div);
-  console.log('add active')
-};
+  };
 
 function reset(){
   $itemName.value = "";
   $itemQuantity.value = "";
   $itemMemo.value ="";
+};
+
+function save(){
+  const listItems = document.querySelectorAll('.item');
+  let items = [];
+  listItems.forEach(listItem =>{
+    let item ={
+      name: listItem.getElementsByClassName('name')[0].innerText,
+      quantity: listItem.getElementsByClassName('quantity')[0].innerText,
+      tag: listItem.getElementsByClassName('tag')[0].innerText,
+      memo: listItem.getElementsByClassName('memo')[0].innerText,
+      completed: listItem.getElementsByClassName('name')[0].classList.contains('text-decoration-line-through')
+    };
+    items.push(item);
+    localStorage.setItem('items', JSON.stringify(items));
+  });
 };
